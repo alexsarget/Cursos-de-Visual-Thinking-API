cfe99564ebce6b055ee465c699df8f4b3b7cf404
Alejandro Tonatiuh <53270727+alexsarget@users.noreply.github.com>
2022-05-03 00:41:04 -0500

    8) Depurando codigo con linter

--- test/VisualPartner.test.js
+++ test/VisualPartner.test.js
@@ -1,11 +1,11 @@
 
-const VisualPartnerController=require("./../lib/controllers/VisualPartnerController")
-const VisualPartnerServices=require("./../lib/services/VisualPartnersService")
+const VisualPartnerController=require("./../lib/controllers/VisualPartnerController");
+const VisualPartnerServices=require("./../lib/services/VisualPartnersService");
 
 
 describe("Test Para Visual PartnerService",()=>{
     test("Requerimiento 1: Obtencion de los emails de los explorers con certificación",()=>{
-        const email=VisualPartnerController.getEmail(true)
+        const email=VisualPartnerController.getEmail(true);
         expect(email).toContain("Todd@visualpartnership.xyz");
     });
 
@@ -16,36 +16,36 @@ describe("Test Para Visual PartnerService",()=>{
             "email": "Sexton@visualpartnership.xyz",
             "credits": 677,
             "enrollments": [
-              "Visual Thinking Avanzado"
+                "Visual Thinking Avanzado"
             ],
             "previousCourses": 6,
             "haveCertification": true
-          },
-          {
+        },
+        {
             "id": "6264d5d8cda17de0d2e9f236",
             "name": "Fuentes",
             "email": "Sharlene@visualpartnership.xyz",
             "credits": 210,
             "enrollments": [
-              "Visual Thinking Avanzado"
+                "Visual Thinking Avanzado"
             ],
             "previousCourses": 10,
             "haveCertification": true
-          }
-        ]
+        }
+        ];
         const result=[{
             "id": "6264d5d85cf81c496446b67f",
             "name": "Lucinda",
             "email": "Sexton@visualpartnership.xyz",
             "credits": 677,
             "enrollments": [
-              "Visual Thinking Avanzado"
+                "Visual Thinking Avanzado"
             ],
             "previousCourses": 6,
             "haveCertification": true
-          }]
+        }];
 
-        const credit=VisualPartnerServices.ConsultCredits(partners,500)
+        const credit=VisualPartnerServices.ConsultCredits(partners,500);
         expect(credit).toStrictEqual(result);
     });
 
